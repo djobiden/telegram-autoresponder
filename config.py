f4c1e4ad11864b655a8fef101679eee33e3ab76e
djobiden <swetickurtash744@gmail.com>
2025-03-14 11:54:41 +0200

    cfg.json removed, config.py added, and main.py modified

--- config.py
+++ config.py
@@ -1,17 +1,5 @@
-# Autoresponder for Telegram
-## Simple autoresponder for Telegram 
-
-### Installation 
-```
-None
-```
-
-### setup-file
-> config.py
-```py
 api_id=None # api_id
 api_hash=None # api_hash
 name ="bot_name" # bot name
 message="**Autoreply**\nHello! My owner is currently busy. Please wait for their response. All the best!" # message to autoreply
 timeout=600 # timeout(in seconds) RECOMENDED > 400
-```
--- main.py
+++ main.py
@@ -1,36 +1,28 @@
 from telethon import TelegramClient, events
+from config import *
 import json
 import time
 
-print('\033[31m Loading cfg.json \033[0m')
-with open('cfg.json', 'r') as cfg:
-  cfg = json.load(cfg)
-  
-  # cfg.json variables
-  api_id = cfg['api_id']
-  api_hash = cfg['api_hash']
-  name = cfg['name']
-  message = cfg['message']
-  timeout = cfg['TIMEOUT']
-  
-  if not api_id and not api_hash:
-    raise Exception('\033[31mYou didn\'t enter the data into the file. cfg.json\033[0m')
+# Loading config file(Examination)
+print('\033[31m Loading config.py \033[0m')
+if not api_id and not api_hash:
+  raise Exception('\033[31mYou didn\'t enter the data into the file. config.py\033[0m')
 print('\033[32m Loading complete \033[0m')
 
-
+# client
 client = TelegramClient(name, api_id, api_hash)
 all_user = {}
-
 
 # NewMessage events
 @client.on(events.NewMessage(incoming=True))
 async def handler(event):
   if event.is_private:
     cur_time = time.time()
+    sender = await event.get_sender()
     user = event.sender_id
     if user in all_user:
       if cur_time - all_user[user] < timeout:
-        print(f'blocked {user}')
+        print(f'blocked request from user {user} | {sender.username}')
     else:
       await event.reply(message)
       all_user[user] = time.time()
